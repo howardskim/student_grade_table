c1641625e32b8ae0e9a180e8aab7558469a9c81c
Howard Kim <howardkim820@gmail.com>
2018-07-17 19:25:37 -0700

    splicing one object

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ var student_array = [];
 
 function initializeApp() {
     addClickHandlersToElements();
+    getDataFromServer();
 }
 
 function addClickHandlersToElements() {
     $('.addButton').on('click', handleAddClicked)
     $('.cancelButton').on('click', handleCancelClick)
+    $('.getDataFromServerButton').on('click', getDataFromServer);
 }
 
 function handleAddClicked() {
@@ -29,7 +31,6 @@ function addStudent() {
     clearAddStudentFormInputs();
     updateStudentList();
     console.log(student_array)
-
 }
 
 function clearAddStudentFormInputs() {
@@ -39,7 +40,7 @@ function clearAddStudentFormInputs() {
 }
 
 function updateStudentList() {
-    renderStudentOnDom(student_array);
+    renderStudentOnDom(student_array[student_array.length-1]);
     var avgGrade = calculateGradeAverage(student_array);
     $('.avgGrade').text(avgGrade)
 }
@@ -49,7 +50,10 @@ function calculateGradeAverage(array) {
     for (var i = 0; i < array.length; i++) {
         total += parseInt(array[i].grade);
     }
-    var averageGrade = total / array.length;
+    var averageGrade = parseInt(total / array.length);
+    if(isNaN(averageGrade)){
+        averageGrade = 0;
+    }
     return averageGrade;
 }
 
@@ -57,8 +61,7 @@ function handleCancelClick() {
     clearAddStudentFormInputs();
 }
 
-function renderStudentOnDom(array) {
-    for (var i = 0; i < array.length; i++) {
+function renderStudentOnDom(eachStudentObject) {
         var tableRow = $('<tr>');
         var tableName = $('<td>');
         var tableCourse = $('<td>');
@@ -66,22 +69,49 @@ function renderStudentOnDom(array) {
         var tableButton = $('<td>')
         var deleteButton = $('<button>', {
             class: 'btn btn-danger dButton',
-            id: i,
+            id: eachStudentObject.id,
             text: 'Delete'
         });
-        deleteButton.on('click', handleDeleteButton)
+        deleteButton.on('click', function() {
+            this.closest('tr').remove();
+            handleDeleteButton(eachStudentObject);
+        })
         tableButton.append(deleteButton)
-        tableName.text(array[i].name);
-        tableCourse.text(array[i].course);
-        tableGrade.text(array[i].grade);
+        tableName.text(eachStudentObject.name);
+        tableCourse.text(eachStudentObject.course);
+        tableGrade.text(eachStudentObject.grade);
         tableRow.append(tableName, tableCourse, tableGrade, tableButton);
-    }
-    $('tbody').append(tableRow);
+        $('tbody').append(tableRow);
+}
+
+function handleDeleteButton(currentStudent) {
+    student_array.splice(currentStudent, 1)
+    var avgGrade = calculateGradeAverage(student_array);
+    $('.avgGrade').text(avgGrade)
 
 }
-function handleDeleteButton(){
-    this.closest('tr').remove();
-    var deleteIndex = $(this).attr('id');
-    student_array.splice(deleteIndex, 1)
 
+function getDataFromServer() {
+    var the_data = {
+        api_key: '7Rc7NsB569'
+    }
+    var ajaxOptions = {
+        dataType: 'json',
+        data: the_data,
+        method: 'POST',
+        url: 'https://s-apis.learningfuze.com/sgt/get',
+        success: function (response) {
+            var responseArray = response.data;
+            console.log(responseArray);
+            student_array = responseArray;
+            for(var i = 0; i < responseArray.length; i++){
+                student_array = responseArray;
+                renderStudentOnDom(responseArray[i]);
+            }
+        },
+        error: function () {
+            console.log('error')
+        }
+    }
+    $.ajax(ajaxOptions);
 }
